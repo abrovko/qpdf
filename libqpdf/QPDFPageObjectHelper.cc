49a39d40ef9651df7e777652d5957ba6c622e05d
abrovko <alexander.brovko@gmail.com>
2021-04-01 07:59:31 -0700

    use cropbox instead of trimbox

--- libqpdf/QPDFPageObjectHelper.cc
+++ libqpdf/QPDFPageObjectHelper.cc
@@ -363,12 +363,13 @@ QPDFPageObjectHelper::getAttribute(std::string const& name,
 QPDFObjectHandle
 QPDFPageObjectHelper::getTrimBox(bool copy_if_shared)
 {
-    QPDFObjectHandle result = getAttribute("/TrimBox", copy_if_shared);
-    if (result.isNull())
-    {
-        result = getCropBox(copy_if_shared);
-    }
-    return result;
+    return getCropBox(copy_if_shared);
+    // QPDFObjectHandle result = getAttribute("/TrimBox", copy_if_shared);
+    // if (result.isNull())
+    // {
+    //     result = getCropBox(copy_if_shared);
+    // }
+    // return result;
 }
 
 QPDFObjectHandle
